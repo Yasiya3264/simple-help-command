93e290de23df83f5688bd63f82e07e47f60b5811
Yasith Kalhara <90952112+Yasiya3264@users.noreply.github.com>
2021-11-07 22:44:07 +0530

    Update help-client.lua

--- help-client.lua
+++ help-client.lua
@@ -1,9 +1,11 @@
 -- This function is used to register a command for the chat. 
 --When this is exectuted, it will execute a function, doing the msg function to the client
 RegisterCommand('help', function()
-    msg("Discord: https://discord.gg/MYr6uTDM")
-    msg("Website: https://teamx.infuze.lk/")
+    msg("Discord: https://discord.gg/MYr6uTDM") -- change discord server invite link to your discord server. If you want any support please join this link.
+    msg("Website: https://teamx.infuze.lk/") -- This our main/official website 
     msg("teamspeak: ts.teamx.ml")
+    msg("To read our server rules and pley")
+    msg("https://domain.com/rules") -- change domain to your website domain this is example domain its not working
 end, false)
 
 -- We declare this 'msg' function on the bottom due to better practices.
